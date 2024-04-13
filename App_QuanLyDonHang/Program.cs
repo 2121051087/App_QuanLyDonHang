2bdf19dc6fcb185f622b15181df5932d25acf59e
Zugoo <135843064+2121051087@users.noreply.github.com>
2024-04-14 01:37:36 +0700

    build: up load database

--- App_QuanLyDonHang/Program.cs
+++ App_QuanLyDonHang/Program.cs
@@ -16,7 +16,7 @@ namespace App_QuanLyDonHang
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form_TrangChu());
+            Application.Run(new Form_DangNhap());
         }
     }
 }
